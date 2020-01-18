e13dd03a5611bf4d9f75bd8dcbc12cea5fe74042
MERT TÜRKMAN <mertturkman@gmail.com>
2020-01-18 13:58:26 +0300

    commit5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello Worldw");
             Console.WriteLine("Hello Worldw");
+            Console.WriteLine("Hello Worldw");
         }
     }
 }
